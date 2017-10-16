cce91b82454df7e89077931a24221df596d702b8
Breo <32192563+breov@users.noreply.github.com>
2017-10-16 11:15:36 +0200

    Prueba Git 1

---
+++ Module1.vb
@@ -0,0 +1,19 @@
+﻿Option Strict On
+Module Module1
+
+    Sub Main()
+        Dim x, y, auxiliar As Integer
+        x = 7
+        y = 16
+        auxiliar = x
+        x = y
+        y = auxiliar
+
+        Console.WriteLine("El numero x es " & x)
+        Console.ReadLine()
+        Console.WriteLine("El numero y es " & y)
+        Console.ReadLine()
+
+    End Sub
+
+End Module
